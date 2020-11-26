c271025eea5ebf48ea0328ecb9e5ec70dcfcc004
Cameron <camglowicki@hotmail.com>
2020-11-26 06:30:59 +0000

    function to check if objects are equal

---
+++ eqObjects.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🧨🧨🧨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  
+  let objK1 = Object.keys(object1);
+  let objK2 = Object.keys(object2);
+
+  if (objK1.length !== objK2.length) {
+    return false;
+  } else {
+    
+    let keys = objK1;
+
+    for (key of keys) {
+      if (eqArrays(objK1, objK2)) {
+        return true;
+      }
+      if (object1[key] !== object2[key]) {
+      return false;
+      }
+    }
+    return true;
+  }
+};
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
