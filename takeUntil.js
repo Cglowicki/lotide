ac71425c399bbbed09a90f85a1982052b695f677
Cameron <camglowicki@hotmail.com>
2020-11-27 01:49:24 +0000

    updated takeUntil.js with assertArraysEqual function

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,29 @@
+const eqArrays = function(arr1, arr2) {
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+    
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+
+  if (!eqArrays(arr1, arr2)) {
+    console.log('🧨🧨🧨 Assertion Failed: arr1 !== arr2');
+
+  } else {
+    console.log('💎💎💎 Assertion Passed: arr1 === arr2');
+  }
+};
+
 const takeUntil = function(array, callback) {
   
   array.forEach((placement, index) => { //passes on each iteration
