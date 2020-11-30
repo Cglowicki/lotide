ff2ad5ef8dae4f62b256558ffd80334ac74b788b
Cameron <camglowicki@hotmail.com>
2020-11-30 21:42:26 +0000

    modularized tail for access to other files

--- tail.js
+++ tail.js
@@ -1,18 +1,9 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual === expected) {
-    console.log(`💎💎💎 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🧨🧨🧨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const tail = function(words) {
   let arr = words.slice(1);
   return arr;
   };
 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+  module.exports = tail;
