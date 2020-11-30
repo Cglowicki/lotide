9646bf0b7fb57553b034d6f4b4d195e64a5d6ed3
Cameron <camglowicki@hotmail.com>
2020-11-30 20:53:11 +0000

    test conditions for assertEqual

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello, World", "hello world");
+assertEqual(10, 56);
+assertEqual("Cam G", "Cam G");
